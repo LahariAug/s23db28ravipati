52413f850f94c94a867019a1fbd74363238fc1f1
LahariAug <100933095+LahariAug@users.noreply.github.com>
2023-11-15 22:41:24 -0600

    Added

--- controllers/hats.js
+++ controllers/hats.js
@@ -41,8 +41,18 @@ exports.hats_create_post = async function(req, res) {
 //res.send('NOT IMPLEMENTED: Hats create POST');
 //};
 // Handle Hat delete form on DELETE.
-exports.hats_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Hats delete DELETE ' + req.params.id);
+exports.hats_delete = async function(req, res) {
+//res.send('NOT IMPLEMENTED: Hats delete DELETE ' + req.params.id);
+console.log("delete " + req.params.id)
+try {
+result = await Hat.findByIdAndDelete( req.params.id)
+console.log("Removed " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": Error deleting ${err}}`);
+}
+
 };
 // Handle Hat update form on PUT.
 exports.hats_update_put = async function(req, res) {
@@ -78,5 +88,60 @@ exports.hats_view_all_Page = async function(req, res) {
         res.status(500);
         res.send(`{"error": ${err}}`);
     }
-
 };
+
+    // Handle a show one view with id specified by query
+exports.hats_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+        result = await Hat.findById( req.query.id)
+        res.render('hatsdetail',{ title: 'Hats Detail', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+ };
+ // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.hats_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('hatscreate', { title: 'Hats Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle building the view for updating a costume.
+// query provides the id
+exports.hats_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Hat.findById(req.query.id)
+    res.render('hatsupdate', { title: 'Hats Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle a delete one view with id from query
+exports.hats_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Hat.findById(req.query.id)
+    res.render('hatsdelete', { title: 'Hats Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+   
+    
+    
+
--- routes/hats.js
+++ routes/hats.js
@@ -3,4 +3,13 @@ const hats_controlers= require('../controllers/hats');
 var router = express.Router();
 /* GET Hats */
 router.get('/', hats_controlers.hats_view_all_Page );
+/* GET detail Hats page */
+router.get('/detail', hats_controlers.hats_view_one_Page);
+/* GET create Hats page */
+router.get('/create', hats_controlers.hats_create_Page);
+/* GET create update page */
+router.get('/update', hats_controlers.hats_update_Page);
+/* GET delete Hats page */
+router.get('/delete', hats_controlers.hats_delete_Page);
+
 module.exports = router;
